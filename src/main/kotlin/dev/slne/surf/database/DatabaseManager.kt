f617646923b6142bcd139b03aa374b29eea2238b
Ammo <ammo@slne.dev>
2025-05-31 19:30:46 +0200

    fix: update configuration type in DatabaseManager to use ConnectionConfig

--- src/main/kotlin/dev/slne/surf/database/DatabaseManager.kt
+++ src/main/kotlin/dev/slne/surf/database/DatabaseManager.kt
@@ -1,7 +1,6 @@
 package dev.slne.surf.database
 
 import dev.slne.surf.database.config.ConnectionConfig
-import dev.slne.surf.database.config.database.DatabaseConfig
 import dev.slne.surf.database.database.DatabaseProvider
 import dev.slne.surf.database.redis.RedisProvider
 import dev.slne.surf.surfapi.core.api.config.createSpongeYmlConfig
@@ -17,7 +16,7 @@ class DatabaseManager(private val configDirectory: Path, private val storageDire
     val redisProvider = RedisProvider(connectionConfig)
 
     init {
-        surfConfigApi.createSpongeYmlConfig<DatabaseConfig>(
+        surfConfigApi.createSpongeYmlConfig<ConnectionConfig>(
             configDirectory,
             "connection-config.yml"
         )
