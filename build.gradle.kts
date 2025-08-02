d2007e76ee3a89d4adf60b92dba0fbf8f4388dd9
Ammo <ammo@slne.dev>
2025-08-02 14:46:55 +0200

    chore: remove unused test dependencies and update Java language level to 24

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
 import dev.slne.surf.surfapi.gradle.util.slneReleases
-import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 
 plugins {
     id("dev.slne.surf.surfapi.gradle.core")
@@ -20,26 +19,6 @@ dependencies {
 
     runtimeOnly(libs.sqlite)
     runtimeOnly(libs.mariadb)
-
-    testImplementation(platform("org.junit:junit-bom:5.13.4"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    testImplementation(kotlin("test"))
-    testImplementation(kotlin("serialization"))
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactive:1.10.2")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.8.22")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.22")
-    testImplementation("dev.slne.surf:surf-api-standalone:1.21.7+")
-}
-
-tasks.test {
-    useJUnitPlatform()
-    testLogging {
-        events("passed", "skipped", "failed")
-        showStandardStreams = true
-        exceptionFormat = TestExceptionFormat.FULL
-    }
 }
 
 kotlin {
